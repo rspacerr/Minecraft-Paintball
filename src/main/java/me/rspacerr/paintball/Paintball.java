6270e605961f97c373d71af913f7f31fc38f7b73
rspacerr <definitelynotoberon@gmail.com>
2024-06-15 23:30:26 -0400

    shoot paintballs

--- src/main/java/me/rspacerr/paintball/Paintball.java
+++ src/main/java/me/rspacerr/paintball/Paintball.java
@@ -1,8 +1,12 @@
 package me.rspacerr.paintball;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
+import org.bukkit.*;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Snowball;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Paintball extends JavaPlugin implements Listener {
@@ -13,6 +17,21 @@ public final class Paintball extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(this, this);
 
         Bukkit.broadcastMessage(ChatColor.GREEN + "Paintballs loaded!");
+    }
+
+    @EventHandler
+    public void onShoot(PlayerInteractEvent e) {
+        if (!(e.getAction() == Action.RIGHT_CLICK_AIR) && !(e.getAction() == Action.RIGHT_CLICK_BLOCK)) { return; }
+
+        Player player = e.getPlayer();
+        if (player.getGameMode().equals(GameMode.SPECTATOR)) return;
+
+        /* shoot paintballs */
+        if (player.getInventory().getItemInMainHand().getType() == Material.DIAMOND_HORSE_ARMOR) {
+            Snowball proj = player.launchProjectile(Snowball.class);
+            proj.setVelocity(proj.getVelocity().multiply(1.25));
+            player.playSound(player.getLocation(), Sound.ENTITY_ITEM_PICKUP, 1, 2);
+        }
     }
 
     @Override
